748d04bb1f4749b3b9bb9222f4b1d0a12fea4ef8
kmicic91 <bilicz.adrian@gmail.com>
2017-08-24 11:57:14 -0400

    session-client

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('client-sessions')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -30,6 +31,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  cookieName: 'session',
+  duration: 24*60*60*1000,
+  secret: 'asdsadf',
+  activeDuration: 30*60*1000
+}))
 
 app.use('/', index);
 app.use('/api', api);
--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var controllers =require('../controllers')
+var bcrypt = require('bcryptjs')
 
 
 router.post('/register', function(req,res,next){
@@ -14,6 +15,65 @@ router.post('/register', function(req,res,next){
 	})
 	.catch(function(err){
 		next(err)
+	})
+})
+
+router.get('/currentuser', function(req,res,next){
+	if(req.session == null){
+		res.json({
+			confimation: 'success',
+			user: null
+		})
+		return
+	}
+	if(req.session.user == null){
+		res.json({
+			confimation: 'success',
+			user: null
+		})
+		return
+	}
+	res.json({
+		confimation: 'succes',
+		profile: req.session.user
+	})
+})
+
+
+router.post('/login', function(req,res,next){
+	var formData = req.body //email, pasword
+
+	controllers.profile
+	.get({email: formData.email},true)
+	.then(function(profiles){
+		
+		if(profiles.length == 0){
+			
+			res.json({
+				confimation: 'fail',
+				profile: 'Profile not found'
+			})
+			return
+		}
+		var profile = profiles[0]
+		var passwordCorrect = bcrypt.compareSync(formData.password,profile.password)
+		if (passwordCorrect == false){
+			req.session.reset
+			res.json({
+				confimation: 'fail',
+				message: 'wrong pass'
+			})
+			return
+		}
+		req.session.user = profile._id.toString()
+		res.redirect('/profile')
+
+	})
+	.catch(function(err){
+		res.json({
+			confimation: 'fail',
+			profile: err
+		})
 	})
 })
 
